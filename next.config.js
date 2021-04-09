e33fbf4d841fc6eec453130f7ce5d3bebe7eda6c
SANTOSH MAHTO <mahtos480@gmail.com>
2021-04-09 21:03:21 +0530

    assest issue

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
+
 module.exports = {
   future: {
     webpack5: true,
   },
+  assetPrefix: ghPages ? '/insantoshmahto.github.io/' : '' 
 };
