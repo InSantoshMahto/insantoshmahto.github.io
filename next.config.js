f9095dcedbf038b6978f4086fb8babb45f42e73f
SANTOSH MAHTO <mahtos480@gmail.com>
2021-04-09 22:07:03 +0530

    debug again

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const isGhPages = process.env.DEPLOY_TARGET === 'gh-pages';
+const isGhPages = process.env.DEPLOY_TARGET === "gh-pages";
 
 module.exports = {
   future: {
     webpack5: true,
   },
-  assetPrefix: isGhPages ? 'https://insantoshmahto.github.io/' : '' 
+  assetPrefix: isGhPages ? "https://insantoshmahto.github.io/" : "",
 };
