aefbe83d8d2e8d14b2d0c13860ce7a7f67449752
SANTOSH MAHTO <mahtos480@gmail.com>
2022-01-10 21:57:41 +0530

    color changed

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 const isGhPages = process.env.DEPLOY_TARGET === "gh-pages";
 
 module.exports = {
-  future: {
-    webpack5: true,
-  },
   assetPrefix: isGhPages ? "https://insantoshmahto.github.io/" : "",
 };
